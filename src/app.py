8549a4aea52a7f7e59a8d4c9856177977c4f25fb
PabloCorzo <pablo.corzocorella@usp.ceu.es>
2025-09-24 22:06:20 +0200

    Esqueleto del flask

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request, redirect
+from dotenv import load_dotenv
+import os
+app = Flask(__name__)
+
+load_dotenv()
+
+
+#get to see a route, post to send the data
+@app.route('/',methods = ['POST','GET'])
+def index():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html'),200
+
+@app.route("/login",methods = ['POST','GET'])
+def login():
+    if request.method == 'POST':
+        return render_template('login.html')
+
+@app.route("/signup",methods = ['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        return render_template('signup.html')
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
