102e48979ff6fad8730726e8bfe38528de5f4779
IvanRengelFerreira <ivan.rengelferreira@usp.ceu.es>
2025-11-13 13:16:28 +0100

    arreglando merge

--- src/model.py
+++ src/model.py
@@ -21,12 +21,11 @@ class Users(db.Model):
     password = db.Column('password', db.String(64), nullable=False)
     coins = db.Column('coins', db.Integer, default=0, nullable=False)
 
-    def __init__(self, username, email, password, coins):
+    def __init__(self, username, email, password,coins =  0):
         self.username = username
         self.email = email
         self.password = hashPassword(password)
-        self.coins = 0
-
+        self.coins = coins
 class Task(db.Model):
     _id = db.Column('task_id', db.Integer, primary_key=True,unique = True)
     name = db.Column('name', db.String(32), unique=False, nullable=False)
